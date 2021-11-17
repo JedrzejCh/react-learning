a6b166056746c09e06136653cfaccd142d8545f2
Jędrzej  Choniawko <jchoniawko@gmail.com>
2021-11-17 22:34:29 +0100

    nav component

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,82 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from './UsersList.styles';
 import FormField from 'components/molecules/formField/FormField';
 import { StyledTitle } from './UsersList.styles';
 import { Button } from 'components/atoms/Button/Button';
-const mockAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (usersData) {
-        resolve([...usersData]);
-      } else {
-        reject({ message: 'Error' });
-      }
-    }, 2000);
-  });
-};
 
-const initialFormValues = {
-  name: '',
-  attendance: '',
-  average: '',
-};
-
-const UsersList = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setLoadingState] = useState([]);
-  const [formValues, formValuesChange] = useState(initialFormValues);
-
-  useEffect(() => {
-    setLoadingState(true);
-    mockAPI()
-      .then((data) => {
-        setUsers(data);
-        setLoadingState(false);
-      })
-      .catch((er) => console.log(er));
-
-    // ComponentWillUnmount:
-    // return ()=> {
-    // window.removeEventListener()
-    // }
-  }, []);
-
-  useEffect(() => {}, [isLoading]);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
-  const handleInputValue = (e) => {
-    formValuesChange({ ...formValues, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newUser = {
-      name: formValues.name,
-      attendance: formValues.attendance,
-      average: formValues.average,
-    };
-
-    setUsers([newUser, ...users]);
-
-    //reset pól formularza
-    formValuesChange(initialFormValues);
-  };
-
+const UsersList = ({ users, isLoading, deleteUser }) => {
   return (
     <>
-      <Wrapper as="form" onSubmit={handleSubmit}>
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputValue} />
-        <FormField label="Attendance" id="attendance" name="attendance" value={formValues.attendance} onChange={handleInputValue} />
-        <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputValue} />
-        <Button type="submit">Add</Button>
-      </Wrapper>
       <Wrapper>
         <StyledTitle>{isLoading ? 'Loading...' : 'List'}</StyledTitle>
         <StyledList>
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,7 +1,10 @@
 import UsersList from 'components/organisms/UsersList/UsersList';
-import React from 'react';
+import Form from 'components/organisms/form/Form';
 import styled, { ThemeProvider } from 'styled-components';
+import { users as usersData } from 'data/users';
+import React, { useState, useEffect } from 'react';
 import { GlobalStyle } from 'assets/styles/globalStyle';
+import { Menu } from 'components/organisms/Menu/Menu';
 import { theme } from 'assets/styles/theme';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 const Wrapper = styled.div`
@@ -15,19 +18,79 @@ const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.lightGray};
 `;
 
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (usersData) {
+        resolve([...usersData]);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, 2000);
+  });
+};
+
+const initialFormValues = {
+  name: '',
+  attendance: '',
+  average: '',
+};
 const Root = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoadingState] = useState([]);
+  const [formValues, formValuesChange] = useState(initialFormValues);
+
+  useEffect(() => {
+    setLoadingState(true);
+    mockAPI()
+      .then((data) => {
+        setUsers(data);
+        setLoadingState(false);
+      })
+      .catch((er) => console.log(er));
+
+    // ComponentWillUnmount:
+    // return ()=> {
+    // window.removeEventListener()
+    // }
+  }, []);
+
+  useEffect(() => {}, [isLoading]);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
+  };
+
+  const handleInputValue = (e) => {
+    formValuesChange({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      name: formValues.name,
+      attendance: formValues.attendance,
+      average: formValues.average,
+    };
+
+    setUsers([newUser, ...users]);
+
+    //reset pól formularza
+    formValuesChange(initialFormValues);
+  };
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Wrapper>
-          <nav>
-            <Link to="/">chuj</Link>
-            <Link to="/add-user">dup</Link>
-          </nav>
+          {/* <Link to="/add-user">dup</Link>
+            <Link to="/">chuj</Link> */}
+          <Menu />
           <Routes>
-            <Route path="/" element={<UsersList />} exact />
-            <Route path="/add-user" element={<UsersList />} />
+            <Route path="/add-user" element={<Form formValues={formValues} handleSubmit={handleSubmit} handleInputValue={handleInputValue} />} />
+            <Route path="/" element={<UsersList deleteUser={deleteUser} users={users} isLoading={isLoading} />} exact />
           </Routes>
         </Wrapper>
       </ThemeProvider>
